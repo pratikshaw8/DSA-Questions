d03a93e04af07e54688e7dd914e4942d72f554d8
Pratik Shaw <pratikshaw8@gmail.com>
2022-05-23 12:42:32 +0530

    Time: 28 ms (64.73%), Space: 18 MB (75.38%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    bool isValid(vector<vector<char>>& board, int x,int y)
+    {
+        
+        char temp=board[x][y];
+        board[x][y]='.';
+        
+        for(int i=0;i<9;i++)
+        {
+            if(board[i][y]==temp) return false;
+            if(board[x][i]==temp) return false;
+            if(board[3*(x/3)+i/3][3*(y/3) + i%3]==temp) return false;
+        }
+        return true;
+    }
+    
+    
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        int i,j;
+       
+        
+        for(int i=0;i<9;i++)
+        {
+            for(j=0;j<9;j++)
+            {
+                if(board[i][j]!='.' and !isValid(board,i,j))
+                    return false;
+            }
+        }
+        return true;
+}
+};
