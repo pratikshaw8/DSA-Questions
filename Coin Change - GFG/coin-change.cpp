8e11d0e305319568758f919bcbed743b52240507
Pratik Shaw <pratikshaw8@gmail.com>
2022-06-25 19:10:07 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.cpp
+++ Coin Change - GFG/coin-change.cpp
@@ -5,32 +5,23 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    long long int solve(int S[],int m,int n,vector<vector<long long int>>& dp)
-    {
-        if(n==0)
-        return dp[n][m]=1;
-        
-        if(n<0)
-        return 0;
-        
-        if(m<0 and n>0)
-        return 0;
-        
-        if(dp[n][m]!=-1)
-        return dp[n][m];
-        
-        return dp[n][m]=solve(S,m-1,n,dp) + solve(S,m,n-S[m],dp);
-        
-    }
-    
     long long int count(int S[], int m, int n) {
 
         // code here.
-        vector<vector<long long int>> dp(n+1,vector<long long int>(m,-1));
+        vector<long long int> dp(n+1,0);
+        dp[0]=1;
         
-        
-        return solve(S,m-1,n,dp);
-        
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(j-S[i] >=0 and dp[j-S[i]]!=0)
+                {
+                    dp[j] =dp[j] + dp[j-S[i]];
+                }
+            }
+        }
+        return dp[n];
     }
 };
 
