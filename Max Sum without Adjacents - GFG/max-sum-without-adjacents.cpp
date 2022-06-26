0c6a04dca1e4216df51e1133f3d8d1c7f56924d8
Pratik Shaw <pratikshaw8@gmail.com>
2022-06-26 11:50:45 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -11,16 +11,19 @@ public:
 	// calculate the maximum sum with out adjacent
 	int findMaxSum(int *arr, int n) {
 	    // code here
-	    vector<int> dp(n,0);
-	    dp[0]=arr[0];
+
+	    int prev2=0;
+	    int prev1=arr[0];
 	    
 	    for(int i=1;i<n;i++)
 	    {
-	        int inc=dp[i-2]+ arr[i];
-	        int exc=dp[i-1];
-	        dp[i]=max(inc,exc);
+	        int inc=prev2+ arr[i];
+	        int exc=prev1;
+	        int ans=max(inc,exc);
+	        prev2=prev1;
+	        prev1=ans;
 	    }
-	    return dp[n-1];
+	    return prev1;
 	}
 };
 
