d67320074e7d4d89180c752b5bd73c53b4e34db1
Pratik Shaw <pratikshaw8@gmail.com>
2022-06-26 19:30:14 +0530

    Added solution - LeetHub

--- Painting the Fence - GFG/painting-the-fence.cpp
+++ Painting the Fence - GFG/painting-the-fence.cpp
@@ -22,19 +22,23 @@ class Solution{
     long long tabulation(int n,int k)
     {
         vector<long long> dp(n+1,0);
-        dp[1]=k;
-        dp[2]=(k*k)%mod;
+        long long prev2=k%mod;
+        long long prev1=(k*k)%mod;
 
         for(int i=3;i<=n;i++)
         {
-            dp[i]=((k-1)* (dp[i-1] +dp[i-2])%mod)%mod;
+            long long ans=((k-1)* (prev1 +prev2)%mod)%mod;
+            prev2=prev1;
+            prev1=ans;
         }
-        return dp[n];
+        return prev1;
     }
     long long countWays(int n, int k){
         // code here
         // vector<long long > dp(n+1,-1);
         // return solve(n,k,dp);
+        if(n==1)
+        return k;
         return tabulation(n,k);
     }
 };
