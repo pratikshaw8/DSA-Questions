a1ef76bad71edb5576b89d1c1b913f3cc7ae8a8d
Pratik Shaw <pratikshaw8@gmail.com>
2022-07-03 19:19:23 +0530

    Added solution - LeetHub

--- BBT counter - GFG/bbt-counter.cpp
+++ BBT counter - GFG/bbt-counter.cpp
@@ -17,11 +17,30 @@ class Solution {
         
         return dp[h] =(solve(h-1,dp) *( solve(h-1,dp)%mod + (2 * (solve(h-2,dp)%mod))%mod)%mod)%mod;
     }
+    int tabulation(int h)
+    {
+        int mod=1e9+7;
+        vector<long> dp(h+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        
+        for(int i=2;i<=h;i++)
+        {
+            dp[i]= (dp[i-1] *( (dp[i-1]%mod + (2* dp[i-2])%mod )%mod))%mod;
+        }
+        return dp[h];
+    }
     long long int countBT(int h) { 
         // code here
+        /*
         vector<long> dp(h+1,-1);
         int child=pow(2,h-1);
         return solve(h,dp);
+        */
+        
+        return tabulation(h);
+        
+        
         
     }
 };
