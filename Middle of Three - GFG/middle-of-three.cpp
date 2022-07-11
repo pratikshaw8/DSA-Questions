ba2976c8eee284a572350e476080c0d0db6db7ff
Pratik Shaw <pratikshaw8@gmail.com>
2022-07-11 11:40:26 +0530

    Added solution - LeetHub

---
+++ Middle of Three - GFG/middle-of-three.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int middle(int A, int B, int C){
+        //code here//Position this line where user code will be pasted.
+        
+        long long int a=A,b=B,c=C;
+       if((a-b)*(b-c)>=0){
+           return B;
+       }
+       else if((b-a)*(a-c)>=0){
+           return A;
+       }
+       else {
+           return C;
+       }
+    }
+};
+
+// { Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A,B,C;
+        cin>>A>>B>>C;
+        Solution ob;
+        cout<<ob.middle(A,B,C) <<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
