0f8e9183127429a61703f28b7d0f519af7d8d40e
Pratik Shaw <pratikshaw8@gmail.com>
2022-07-23 14:07:20 +0530

    Time: 27 ms (61.06%), Space: 17.8 MB (99.89%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -3,12 +3,11 @@ public:
     int missingNumber(vector<int>& nums) {
         
         int ans=0;
-        int i=0;
         
-        for(i=0;i<nums.size();i++)
+        for(int i=0;i<nums.size();i++)
         {
-            ans= ans xor nums[i];
-            ans=ans xor i+1;
+            ans = ans xor nums[i];
+            ans = ans xor i+1;
         }
         return ans;
     }
