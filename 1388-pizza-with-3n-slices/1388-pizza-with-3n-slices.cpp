9a3d36550e2a205cb4de5faf1a394add88dc8b6e
Pratik Shaw <pratikshaw8@gmail.com>
2022-07-26 22:07:28 +0530

    Time: 59 ms (14.04%), Space: 15.9 MB (12.33%) - LeetHub

--- 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
+++ 1388-pizza-with-3n-slices/1388-pizza-with-3n-slices.cpp
@@ -13,12 +13,52 @@ public:
         
         return dp[stIdx][n] = max(take,notTake);
     }
+    int solveTab(vector<int>& slices)
+    {
+        int k=slices.size();
+        
+        vector<vector<int>> dp(k+2,vector<int>(k+2,0));
+        
+        for(int index= k-2 ; index>=0 ; index--)
+        {
+            for(int n = 1 ; n <= k/3 ; n++)
+            {
+                int take = slices[index] + dp[index+2][n-1];
+                int notTake = 0 + dp[index+1][n];
+                
+                dp[index][n] = max(take,notTake);
+            }
+        }
+        int case1= dp[0][k/3];
+        
+        for(int i=0;i<k;i++)
+        {
+            fill(dp[i].begin() , dp[i].end() ,0);
+        }
+        
+        for(int index= k-1 ; index>=1 ; index--)
+        {
+            for(int n = 1 ; n <= k/3 ; n++)
+            {
+                int take = slices[index] + dp[index+2][n-1];
+                int notTake = 0 + dp[index+1][n];
+                
+                dp[index][n] = max(take,notTake);
+            }
+        }
+        int case2= dp[1][k/3];
+        
+        return max(case1,case2);
+        
+    }
     int maxSizeSlices(vector<int>& slices) {
+        /*
         int k=slices.size();
         int n=k/3;
         vector<vector<int>> dp(k,vector<int>(k,-1));
         
         int case1 = solve(0,k-2,slices,k/3,dp);
+        
         for(int i=0;i<k;i++)
         {
             fill(dp[i].begin() , dp[i].end() ,-1);
@@ -26,5 +66,9 @@ public:
         int case2 = solve(1,k-1,slices,k/3,dp);
         
         return max(case1,case2);
+        */
+        
+        return solveTab(slices);
+        
     }
 };
