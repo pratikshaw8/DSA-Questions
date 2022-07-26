dbff9822b1326c2dd24cec2d5c78c4d70963c42a
Pratik Shaw <pratikshaw8@gmail.com>
2022-07-26 22:35:24 +0530

    Time: 31 ms (72.80%), Space: 6.6 MB (66.98%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -4,10 +4,10 @@ public:
     int numRollsToTarget(int n, int k, int target) {
         
         int mod=1e9+7;
-        vector<vector<int>> dp(n+1, vector<int>(target+1 , 0));
-        
+        vector<int> prev(target+1,0);
+        vector<int> curr(target+1,0);        
         //analyze base case
-        dp[0][0]=1;
+        prev[0] = 1;
         
         
         for(int dices =1 ; dices <=n ; dices++)
@@ -19,12 +19,13 @@ public:
                 for(int i=1; i<=k;i++)
                 {
                     if(x-i >=0)
-                    ans = (ans+ dp[dices-1][x-i])%mod;
+                    ans = (ans+ prev[x-i])%mod;
                 }
                 
-                dp[dices][x] = ans;
+                curr[x] = ans;
             }
+            prev=curr;
         }
-        return dp[n][target];
+        return curr[target];
     }
 };
