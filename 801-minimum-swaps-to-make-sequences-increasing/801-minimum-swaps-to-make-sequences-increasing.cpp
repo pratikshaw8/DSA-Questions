f03a938293b71184057a2485f2d402a611830d92
Pratik Shaw <pratikshaw8@gmail.com>
2022-08-22 23:10:28 +0530

    Time: 763 ms (5.00%), Space: 120.5 MB (40.74%) - LeetHub

--- 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 801-minimum-swaps-to-make-sequences-increasing/801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -21,14 +21,47 @@ public:
             
         return  dp[idx][swapped] = ans;
     }
+    int tabulation(vector<int>& nums1, vector<int>& nums2)
+    {
+        int n=nums1.size();
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        
+        for(int idx = n-1; idx>=1;idx--)
+        {
+            for(int swapped=1; swapped>=0 ;swapped--)
+            {
+                int prev1=nums1[idx-1],prev2=nums2[idx-1];
+        
+                if(swapped)
+                    swap(prev1,prev2);
+
+                int ans=INT_MAX;
+
+                //noswap
+                if(nums1[idx] > prev1 and nums2[idx] > prev2){ 
+                    ans= dp[idx+1][0];
+                }
+
+                if(nums1[idx] > prev2 and nums2[idx] > prev1){
+                    ans= min(ans, 1 + dp[idx+1][1]);
+                }
+
+                dp[idx][swapped] = ans;
+            }
+            
+        }
+        return dp[1][0];
+        
+    }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         
         nums1.insert(nums1.begin(),-1);
         nums2.insert(nums2.begin(),-1);
         int n=nums1.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return tabulation(nums1,nums2);
+//         vector<vector<int>> dp(n,vector<int>(2,-1));
         
-        int count = solve(nums1,nums2,1,0,dp);
-        return count;
+//         int count = solve(nums1,nums2,1,0,dp);
+//         return count;
     }
 };
