3bd1cf8ee6d76c64a042717e7c838bdde9599acb
Pratik Shaw <pratikshaw8@gmail.com>
2022-08-30 10:21:24 +0530

    Time: 49 ms (74.51%), Space: 19.5 MB (39.21%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
+        sort(intervals.begin() , intervals.end());
         
         vector<vector<int>> ans;
-        sort(intervals.begin(),intervals.end());
+
         stack<int> st1;
-        st1.push(intervals[0][0]);
         stack<int> st2;
+        
+        st1.push(intervals[0][0]);
         st2.push(intervals[0][1]);
-        
-        
         
         for(int i=1;i<intervals.size();i++)
         {
-            if(intervals[i][0] <= st2.top() and intervals[i][0] >=st1.top())
+            if(intervals[i][0] <= st2.top() and intervals[i][0] >= st1.top())
             {
-                
-                int top1=st1.top();
+                int top1=st1.top(),top2=st2.top();
                 st1.pop();
-                st1.push(min(top1,intervals[i][0]));
-                
-                int top2=st2.top();
                 st2.pop();
+                
+                st1.push(min(top1,intervals[i][0]));
                 st2.push(max(top2,intervals[i][1]));
-                
-                
             }
             else
             {
@@ -36,13 +31,9 @@ public:
         
         while(!st1.empty())
         {
-            vector<int> temp;
-            temp.push_back(st1.top());
-            temp.push_back(st2.top());
+            ans.push_back({st1.top(),st2.top()});
             st1.pop();
             st2.pop();
-            ans.push_back(temp);
-            temp.clear();
         }
         sort(ans.begin(),ans.end());
         return ans;
