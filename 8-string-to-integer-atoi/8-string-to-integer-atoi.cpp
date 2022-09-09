e2309530c8f3de42d0b0ef2c3c4dcaf83df49a5b
Pratik Shaw <pratikshaw8@gmail.com>
2022-09-09 11:10:35 +0530

    Time: 8 ms (33.19%), Space: 7.1 MB (13.62%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i=0;
+        while(i<s.length() and s[i] ==' ') i++;
+        int sign=1;
+        if(s[i] == '-' || s[i] == '+'){
+            if(s[i] == '-')
+                sign=-1;
+            i++;
+        }
+        
+        int64_t ans=0;
+        while(s[i] >= '0' and s[i] <='9')
+        {
+            ans  =ans *10 + (s[i] - '0');
+            if(ans > INT_MAX)
+            {
+                if(sign == -1) return INT_MIN;
+                return INT_MAX;
+            }
+            i++;    
+        }
+        if(sign==-1) return -1 * (int32_t)ans;
+        
+        return (int32_t)ans;
+    }
+};
